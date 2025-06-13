6cb840f3065062ca4ccdd35bc2afa909578e6090
Emad Adel <aemad9396@gmail.com>
2025-06-13 22:50:13 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-fetch("site_data.json")
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("name").textContent = data.name;
-    document.getElementById("title").textContent = data.title;
-    document.getElementById("linkedin").href = data.linkedin;
-  });
